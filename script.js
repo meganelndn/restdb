131d6cd3291f0d4ba9b84fa1d9a07c0a0446e97c
megane <megane.londono@hotmail.com>
2020-04-14 14:32:53 +0200

    POST function done, saved new data to restdb

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-function get() {
+const endpoint = "https://frontendspring20-34e5.restdb.io/rest/celebrities";
+const apiKey = "5e957564436377171a0c232c";
 
-    fetch("https://frontendspring20-34e5.restdb.io/rest/celebrities?max=10", {
+window.addEventListener("load", (e) => {
+    document.querySelector("button.add-new").addEventListener("click", () => {
+        const data = {
+            name: "Mégane",
+            alias: "Megsipegsi",
+            dob: "1998-06-03",
+            albums: 0,
+            exes: ["Orlando Bloom", "Johnny Depp"]
+        };
+        post(data);
+    });
+});
+
+function get() {
+    document.querySelector("main").innerHTML = "";
+
+    fetch(endpoint + "?max=10", {
             method: "get",
             headers: {
                 "Content-Type": "application/json; charset=utf-8",
-                "x-apikey": "5e957564436377171a0c232c",
+                "x-apikey": apiKey,
                 "cache-control": "no-cache"
             }
         })
         .then((e) => e.json())
         .then(showCelebrities);
 }
+get();
 
 function showCelebrities(data) {
     data.forEach(showCeleb);
@@ -22,7 +40,7 @@ function showCeleb(celebrity) {
     const parent = document.querySelector("main");
 
     copy.querySelector("h1").textContent = celebrity.name;
-    copy.querySelector("h2").textContent = celebrity.alias;
+    copy.querySelector("h2").textContent = "Also known as: " + celebrity.alias;
 
     const ul = copy.querySelector("ul");
     celebrity.exes.forEach((ex) => {
@@ -33,4 +51,21 @@ function showCeleb(celebrity) {
     parent.appendChild(copy);
 }
 
-get();
+function post(data) {
+
+    // OPTIMISTIC INSERTS
+    //showCeleb(data);
+
+    const postData = JSON.stringify(data);
+    fetch(endpoint, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "x-apikey": apiKey,
+                "cache-control": "no-cache"
+            },
+            body: postData,
+        })
+        .then((res) => res.json())
+        .then((data) => showCeleb(data));
+}
