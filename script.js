51941e3ed0a2abded3afa6705ebd0f2c1e2d0669
megane <megane.londono@hotmail.com>
2020-04-14 15:54:28 +0200

    DELETE and PUT functions implemented

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener("load", (e) => {
     document.querySelector("button.add-new").addEventListener("click", () => {
         const data = {
             name: "Mégane",
-            alias: "Megsipegsi",
+            alias: "Megs" + Math.random(),
             dob: "1998-06-03",
             albums: 0,
             exes: ["Orlando Bloom", "Johnny Depp"]
@@ -35,28 +35,32 @@ function showCelebrities(data) {
 }
 
 function showCeleb(celebrity) {
+    console.log(celebrity)
+
     const template = document.querySelector("template").content;
     const copy = template.cloneNode(true);
     const parent = document.querySelector("main");
 
+    copy.querySelector("article").dataset.id = celebrity._id;
     copy.querySelector("h1").textContent = celebrity.name;
     copy.querySelector("h2").textContent = "Also known as: " + celebrity.alias;
-
     const ul = copy.querySelector("ul");
     celebrity.exes.forEach((ex) => {
         const li = document.createElement("li");
         li.textContent = ex;
         ul.appendChild(li)
-    })
+    });
+
+    copy.querySelector("button.delete-me").addEventListener("click", () => deleteIt(celebrity._id));
     parent.appendChild(copy);
 }
 
 function post(data) {
-
-    // OPTIMISTIC INSERTS
-    //showCeleb(data);
+    // OPTIMISTIC INSERTS below
+    // showCeleb(data);
 
     const postData = JSON.stringify(data);
+
     fetch(endpoint, {
             method: "post",
             headers: {
@@ -68,4 +72,55 @@ function post(data) {
         })
         .then((res) => res.json())
         .then((data) => showCeleb(data));
+}
+
+function deleteIt(id) {
+
+    document.querySelector(`article[data-id="${id}"]`).remove()
+
+    fetch(`${endpoint}/${id}`, {
+            method: "delete",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "x-apikey": apiKey,
+                "cache-control": "no-cache"
+            }
+        })
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+}
+
+function put(id) {
+
+    let data = {
+        name: "Mégane3",
+        alias: "Megs3" + Math.random(),
+        dob: "1998-06-01",
+        albums: 0,
+        exes: ["Me", "You"]
+    }
+
+    let postData = JSON.stringify(data);
+
+    fetch(`${endpoint}/${id}`, {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "x-apikey": apiKey,
+                "cache-control": "no-cache"
+            },
+            body: postData
+        })
+        .then((d) => d.json())
+        .then((data) => {
+            const copy = document.querySelector(`article[data-id="${id}"]`)
+            copy.querySelector("h1").textContent = data.name;
+            copy.querySelector("h2").textContent = "Also known as: " + data.alias;
+            const ul = copy.querySelector("ul");
+            data.exes.forEach((ex) => {
+                const li = document.createElement("li");
+                li.textContent = ex;
+                ul.appendChild(li)
+            });
+        });
 }
